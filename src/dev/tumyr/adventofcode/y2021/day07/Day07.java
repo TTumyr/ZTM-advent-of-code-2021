b7c972fce8bd5cf48623780f4d002e25badc785c
TTumyr <trond.tumyr@gmail.com>
2021-12-18 22:43:36 +0100

    day 7

---
+++ src/dev/tumyr/adventofcode/y2021/day07/Day07.java
@@ -0,0 +1,64 @@
+package dev.tumyr.adventofcode.y2021.day07;
+
+import dev.tumyr.controller.FileOperation;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Day07 {
+    private final String data;
+    ArrayList<Integer> horPosition = new ArrayList<Integer>();
+
+    public Day07() throws IOException {
+        data = FileOperation.getTextFile( getClass().getResource("") + "data.txt");
+        convertData();
+    }
+    public String solve() {
+        int partOne = findCheapestPosition("partOne");
+        int partTwo = findCheapestPosition("partTwo");
+        return "Part One: " + partOne + "\n" + "Part Two: " + partTwo;
+    }
+
+    private void convertData() {
+        Scanner scanner = new Scanner(data);
+        while(scanner.hasNextLine()) {
+            String[] strLine = scanner.nextLine().split("\\s*,\\s*");
+            for (String s : strLine) {
+                ArrayList<String> line = new ArrayList<String>();
+                horPosition.add(Integer.parseInt(s));
+            }
+        }
+        scanner.close();
+    }
+
+    private int findCheapestPosition(String mode) {
+        int cheapestHorPos = 0;
+        int maxHorPos = 0;
+        for (Integer integer : horPosition) {
+            if (maxHorPos < integer) maxHorPos = integer;
+        }
+        int[] horPosCost = new int[maxHorPos];
+        for (int i = 0; i < horPosCost.length; i++) {
+            for (Integer integer : horPosition) {
+                if (integer != i) {
+                    if (Objects.equals(mode, "partTwo")) {
+                        if (integer > i) {
+                            for (int k = 0; k < integer - i; k++) {
+                                horPosCost[i] += (1 + k);
+                            }
+                        } else {
+                            for (int k = 0; k < i - integer; k++) {
+                                horPosCost[i] += (1 + k);
+                            }
+                        }
+                    } else horPosCost[i] += (integer > i) ?
+                            (integer - i) : (i - integer);
+                }
+            }
+            if(horPosCost[i] < cheapestHorPos || cheapestHorPos == 0) cheapestHorPos = horPosCost[i];
+        }
+        return cheapestHorPos;
+    }
+}
